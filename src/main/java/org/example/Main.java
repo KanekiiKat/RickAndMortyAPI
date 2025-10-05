a244800a66d2b18a7c7b9a1fcbbdb64429d4ed96
KanekiiCat <alex123viri@gmail.com>
2025-10-05 11:44:56 +0200

    Añadir streams

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ import org.example.model.Character;
 import org.example.storage.*;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,7 +14,34 @@ public class Main {
 
         List<Character> characters = CharacterStorage.loadUrl();
 
-        System.out.println(characters);
+        // Personajes que están vivos y un contador.
+
+        System.out.println("\nPersonajes vivos: \n");
+
+        characters.stream()
+                .filter(c -> c.getStatus().equals("Alive"))
+                .forEach(System.out::println);
+
+        characters.stream()
+                .filter(c -> c.getStatus().equals("Alive"))
+                .collect(Collectors.groupingBy(c -> c.getStatus(), Collectors.counting()))
+                .forEach((vivos, cuenta) -> System.out.println(vivos + " = " + cuenta) );
+
+        // Personajes que están muertos y que son humanos.
+
+        System.out.println("\n\nPersonajes muertos humanos: \n");
+
+        characters.stream()
+                .filter(c -> c.getStatus().equals("Dead") && c.getSpecies().equals("Human"))
+                .forEach(System.out::println);
+
+
+        // Personajes agrupados por especies
+
+        System.out.println("\n\nAgrupados por especies: \n");
+        characters.stream()
+                .collect(Collectors.groupingBy(Character::getSpecies))
+                .forEach((s , v) -> System.out.println("Specie " + s + " = " + v + "\n"));
 
     }
 }
